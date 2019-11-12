5a789ca258a9a615b0af93725d895a10db6f00a8
Lokesh Meher <lokeshmeher34@gmail.com>
2019-11-12 17:12:42 +0530

    Disable explicit port binding in gunicorn

--- server/gunicorn_conf.py
+++ server/gunicorn_conf.py
@@ -1,7 +1,7 @@
 import os
 import multiprocessing
 
-bind = "0.0.0.0:{}".format(os.environ.get('PORT', 8000))
+# bind = "0.0.0.0:{}".format(os.environ.get('PORT', 8000))
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 loglevel = "info"
